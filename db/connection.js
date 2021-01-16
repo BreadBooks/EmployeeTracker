385f1d6bcd7b62be432a1585ab1405ec880a6748
Brett B <65505937+BreadBooks@users.noreply.github.com>
2021-01-16 10:12:06 -0600

    added connection

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "beep",
+  database: "employees"
+});
+
+connection.connect();
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
